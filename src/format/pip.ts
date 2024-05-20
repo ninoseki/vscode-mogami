81e507b2a7673a78438307d5e6089ed9cd76fa79
Manabu Niseki <manabu.niseki@gmail.com>
2024-05-20 09:26:32 +0900

    fix: support dependency name only case

--- src/format/pip.ts
+++ src/format/pip.ts
@@ -15,15 +15,22 @@ const specifierPattern = `${specifierPartPattern}(?:\\s*,${specifierPartPattern}
 const dependencyPattern = `(${packagePattern})(${extrasPattern})(${specifierPattern})`;
 
 export const pkgValRegExp = RegExp(`^${dependencyPattern}$`);
+const packageRegExp = /^[a-z0-9][\w.-]*[a-z0-9]$/i;
 
 export function parse(line: string): DependencyType | undefined {
   const [_line] = line.split("#").map((part) => part.trim());
   const [lineNoEnvMarkers] = _line.split(";").map((part) => part.trim());
   const lineNoHashes = lineNoEnvMarkers.split(" \\")[0];
   const matches = pkgValRegExp.exec(lineNoHashes);
+
   if (!matches) {
+    // line may be a dependency name only (e.g. "requests")
+    if (packageRegExp.test(lineNoEnvMarkers)) {
+      return { name: lineNoHashes };
+    }
     return undefined;
   }
+
   const [, name, , specifier] = matches;
   return { name, specifier: specifier.trim() };
 }
