8f1df1b92477aa58de7a75e5017a6daade87897f
Manabu Niseki <manabu.niseki@gmail.com>
2024-06-08 15:59:06 +0900

    fix: fix prerelease version eq & compare issue (#64)

--- src/versioning/utils.spec.ts
+++ src/versioning/utils.spec.ts
@@ -31,6 +31,8 @@ describe("eq", () => {
     ["1.0.0.1", "1.0.0.2", false],
     ["~ 1.0.0.1", "1.0.0.1", true],
     ["~ 1.0.0.1", "1.0.0.2", false],
+    ["7.2.0.beta2", "7.2.0.beta2", true],
+    ["7.2.0.beta2", "7.2.0.beta1", false],
   ])(
     "eq(%s, %s) === %s",
     (version: string, specifier: string, expected: boolean) => {
@@ -46,6 +48,7 @@ describe("compare", () => {
     ["1.0.0", "0.9.0", 1],
     ["1.0.1", "1.0.1.1", -1],
     ["1.0.1.1", "1.0.1", 1],
+    ["7.2.0.beta1", "7.2.0.beta2", -1],
   ])(
     "eq(%s, %s) === %s",
     (version: string, specifier: string, expected: number) => {
