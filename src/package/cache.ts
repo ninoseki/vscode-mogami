62501c39de178be0fae44682f688e074dea3f363
Manabu Niseki <manabu.niseki@gmail.com>
2024-07-21 10:12:22 +0900

    fix: remove console

--- src/package/cache.ts
+++ src/package/cache.ts
@@ -3,7 +3,5 @@ import { buildMemoryStorage } from "axios-cache-interceptor";
 export const cache = buildMemoryStorage();
 
 export function clearCache() {
-  console.log(cache.data);
   cache.data = {};
-  console.log(cache.data);
 }
