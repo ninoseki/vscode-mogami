5e880ca6217201aff838bd08908232dc364de3ba
Manabu Niseki <manabu.niseki@gmail.com>
2025-09-15 18:23:37 +0900

    feat: support tool.uv.index (#138)

--- src/project/common.ts
+++ src/project/common.ts
@@ -1,5 +1,5 @@
 // forked from https://github.com/Twixes/pypi-assistant/
-import { TOMLNode } from "toml-eslint-parser/lib/ast";
+import { TOMLKeyValue, TOMLNode, TOMLValue } from "toml-eslint-parser/lib/ast";
 import { Visitor } from "toml-eslint-parser/lib/traverse";
 
 import type { DependencyType, RawRangeType } from "@/schemas";
@@ -9,6 +9,7 @@ import { parseLineAsDependency } from "./requirements";
 export class TOMLVisitor implements Visitor<TOMLNode> {
   public pathStack: (string | number)[] = [];
   public dependencies: [DependencyType, RawRangeType][] = [];
+  public source: string | undefined = undefined;
 
   public enterNode(node: TOMLNode) {
     if (node.type === "TOMLTable") {
@@ -22,7 +23,13 @@ export class TOMLVisitor implements Visitor<TOMLNode> {
           "name" in key ? key.name : "value" in key ? key.value : "",
         ),
       );
+
+      this.potentiallyRegisterUvSourceByIndex(node);
       return;
+    }
+
+    if (!this.source && node.type === "TOMLValue") {
+      this.potentiallyRegisterUvSourceByIndexUrl(node);
     }
   }
 
@@ -56,6 +63,49 @@ export class TOMLVisitor implements Visitor<TOMLNode> {
           elem.loc.end.column,
         ],
       ]);
+    }
+  }
+
+  private potentiallyRegisterUvSourceByIndex(node: TOMLKeyValue): void {
+    // Check for "tool.uv.index.url"
+
+    if (
+      this.pathStack[0] === "tool" &&
+      this.pathStack[1] === "uv" &&
+      this.pathStack[2] === "index"
+    ) {
+      const source: string | undefined = (() => {
+        const key = node.key.keys[0];
+        if ("name" in key && key.name === "url") {
+          if (
+            node.value.type === "TOMLValue" &&
+            typeof node.value.value === "string"
+          ) {
+            return node.value.value;
+          }
+        }
+
+        return undefined;
+      })();
+      if (source && !this.source) {
+        this.source = source;
+      }
+    }
+  }
+
+  private potentiallyRegisterUvSourceByIndexUrl(node: TOMLValue): void {
+    // check for "tool.uv.index-url"
+    if (this.pathStack[0] === "tool" && this.pathStack[1] === "uv") {
+      const source: string | undefined = (() => {
+        if ((this.pathStack[2] as string) === "index-url") {
+          return node.value.toString();
+        }
+        return undefined;
+      })();
+
+      if (source && !this.source) {
+        this.source = source;
+      }
     }
   }
 }
